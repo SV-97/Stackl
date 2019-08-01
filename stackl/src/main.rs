4c7065a21953e2e2b6ffc83a46568c7a7afe35c5
Stefan Volz <volzstefan97@googlemail.com>
2019-08-01 23:23:37 +0200

    Added REPL

--- stackl/src/main.rs
+++ stackl/src/main.rs
@@ -3,21 +3,23 @@ mod tokenizer;
 #[macro_use]
 #[allow(dead_code)]
 mod fancyterm;
+mod ast;
+mod functional;
 mod parser;
 mod reporter;
-mod functional;
-mod ast;
-
+mod shell;
 
 use fancyterm::*;
 use prelude::*;
 use reporter::*;
+use shell::Shell;
 use tokenizer::*;
 
 use std::env::args;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::rc::Rc;
 
 /// Read a the contents of a file relative to the project's root directory
 fn read_file(filename: &str) -> String {
@@ -30,23 +32,55 @@ fn read_file(filename: &str) -> String {
     source
 }
 
-fn main() {
-    let mut args = args().collect::<Vec<String>>();
-    args.push("/home/sv-97/GitHub/Stackl/stackl/examples/src2.stackl".to_string()); // only for debugging
-    let filename = &args[1];
+fn handle_file(filename: &str) {
     let source = read_file(filename);
 
-    let source = Source::new(filename.clone(), source);
-    let logger = Logger::new(source.clone());
-    let tokens = Tokenizer::new(source.clone(), Some(logger));
-    for tok
-    
-     in tokens {
+    let source = Rc::new(Source::new(filename.to_string(), source));
+    let logger = Logger::new(Rc::clone(&source));
+    let tokens = Tokenizer::new(Rc::clone(&source), Some(logger));
+    for tok in tokens {
         println!(
             "{}{} {:?}",
             tok,
             colored!(":", params!(Modifier::Faint)),
             source.from_span(&tok.span)
         );
+    }
+}
+
+fn handle_repl() {
+    let mut repl = Shell::new("Stackl");
+    repl.set_ansi(params!(), params!(Color::Blue, Modifier::Faint), params!());
+
+    let source = Rc::new(Source::new("Interactive".to_string(), "".to_string()));
+    let mut tokens = Tokenizer::new(source, None);
+    for input in 0.. {
+        repl.set_seperators(
+            &format!("In[{:?}]: ", input),
+            &format!("Out[{:?}]: ", input),
+        );
+        let text = repl.read();
+        let source = Rc::new(Source::new("Interactive".to_string(), text));
+        tokens.set_source(Rc::clone(&source));
+        tokens.reset();
+        for tok in tokens.clone() {
+            repl.writeln(format!(
+                "{}{} {:?}",
+                tok,
+                colored!(":", params!(Modifier::Faint)),
+                source.from_span(&tok.span)
+            ));
+        }
+    }
+}
+
+fn main() {
+    let mut args = args().collect::<Vec<String>>();
+    // args.push("/home/sv-97/GitHub/Stackl/stackl/examples/src2.stackl".to_string()); // only for debugging
+    let filename = &args.get(1);
+
+    match filename {
+        Some(name) => handle_file(name),
+        _ => handle_repl(),
     }
 }

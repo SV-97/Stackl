37ec6b443cac63bda8dcf958f340dea27652bdf5
Stefan Volz <volzstefan97@googlemail.com>
2019-08-01 22:16:54 +0200

    Basic AST and TryFrom for UnOps

--- stackl/src/ast.rs
+++ stackl/src/ast.rs
@@ -1,5 +1,7 @@
 use super::prelude::*;
-use super::tokenizer::Token;
+use super::tokenizer::{Tok, Token};
+
+use std::convert::TryFrom;
 
 trait NodeVisitor {
     fn visit<T>(node: Node) -> T;
@@ -20,6 +22,9 @@ pub enum NodeType {
         operation: UnOp,
         val: Box<Node>,
     },
+    Program {
+        expressions: Vec<Node>,
+    },
 }
 
 pub enum BinOp {
@@ -35,4 +40,43 @@ pub enum BinOp {
 pub enum UnOp {
     Not,
     Minus,
+}
+
+impl TryFrom<Tok> for UnOp {
+    type Error = String;
+
+    fn try_from(tok: Tok) -> Result<Self, Self::Error> {
+        match tok {
+            Tok::Not => Ok(UnOp::Not),
+            Tok::Sub => Ok(UnOp::Minus),
+            x => Err(format!("Failed conversion of Token {:?} to ast::UnOp", x)),
+        }
+    }
+}
+
+impl Node {
+    pub fn new(span: Span, node: NodeType) -> Self {
+        Node { span, node }
+    }
+}
+
+impl NodeType {
+    pub fn program(expressions: Vec<Node>) -> Self {
+        NodeType::Program { expressions }
+    }
+
+    pub fn binary_operation(operation: BinOp, left: Node, right: Node) -> Self {
+        NodeType::BinaryOp {
+            operation,
+            left: Box::new(left),
+            right: Box::new(right),
+        }
+    }
+
+    pub fn unary_operation(operation: UnOp, val: Node) -> Self {
+        NodeType::UnaryOp {
+            operation,
+            val: Box::new(val),
+        }
+    }
 }
